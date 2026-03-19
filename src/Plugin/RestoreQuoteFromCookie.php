b1bb85fd57497c949a5914253cefe10cf454ffb1
Sergiu Moscalu <sergiu@nordicwebteam.se>
2026-03-19 13:23:13 +0100

    WAL-15 fix error Rejected by the server due to a business error. Purchase_Commitment_Found

--- src/Plugin/RestoreQuoteFromCookie.php
+++ src/Plugin/RestoreQuoteFromCookie.php
@@ -5,6 +5,7 @@ use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Model\ResourceModel\Quote\CollectionFactory as QuoteCollectionFactory;
 use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class RestoreQuoteFromCookie
 {
@@ -12,17 +13,23 @@ class RestoreQuoteFromCookie
     protected $quoteRepository;
     protected $quoteCollectionFactory;
     protected $cookieManager;
+    private $orderManager;
+    private $configFactory;
 
     public function __construct(
         CheckoutSession $checkoutSession,
         CartRepositoryInterface $quoteRepository,
         QuoteCollectionFactory $quoteCollectionFactory,
-        CookieManagerInterface $cookieManager
+        CookieManagerInterface $cookieManager,
+        \Webbhuset\CollectorCheckout\Checkout\Order\ManagerFactory $orderManager,
+        \Webbhuset\CollectorCheckout\Config\ConfigFactory $configFactory,
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->quoteRepository = $quoteRepository;
         $this->quoteCollectionFactory = $quoteCollectionFactory;
         $this->cookieManager = $cookieManager;
+        $this->orderManager = $orderManager;
+        $this->configFactory = $configFactory;
     }
 
     public function beforeExecute($subject)
@@ -42,8 +49,19 @@ class RestoreQuoteFromCookie
                 ->setOrder('entity_id', 'DESC')
                 ->getFirstItem();
 
+            try {
+                $order = $this->orderManager->create()->getOrderByPublicToken($publicId);
+                $config = $this->configFactory->create(['storeId' => $quote->getStoreId()]);
+                $acknowledged  = $config->getOrderStatusAcknowledged();
+                if ($order->getStatus() == $acknowledged) {
+                    return null;
+                }
+            } catch (NoSuchEntityException $e) {
+
+            }
+
             if ($quote->getId()) {
-                $quote->setIsActive(1);
+                $quote->setIsActive(1)->setReservedOrderId(null);
                 $this->quoteRepository->save($quote);
                 
                 $this->checkoutSession->replaceQuote($quote);
